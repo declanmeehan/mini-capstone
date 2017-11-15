daa88e9497edbf6d846a7e48cec933d182e3a166
Declan <declan.meehan@gmail.com>
2017-11-14 18:51:20 -0600

    finally added all the products, very proud of myself

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ Product.create({"name" => "six dollars", "price" => "5.99", "image" => "http://www.yatahoy.com/wp-content/uploads/2014/07/Six-dollars.jpg" , "description" => "six dollars is now going for 5.99!" })
+Product.create({"name" => "99 cents", "price" => "$20" , "image" => "https://www.bargainballoons.com/products/mylarballoonsStarBurst99cent.gif" , "description" => "99 cents in one of our most popular product and goes great with any dollar amount." })
+Product.create({"name" => "10%", "price" => "5.29", "image" => "https://www.colourbox.com/preview/4595117-10-percent.jpg", "description" => "10 percent is a great purchase if you like taxes" })
+
