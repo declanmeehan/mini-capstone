74379825c19f3344f914913aa8c109e0eed62b08
Declan <declan.meehan@gmail.com>
2017-11-14 19:21:50 -0600

    added seprate routes for each product first bonus done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,16 @@ class ProductsController < ApplicationController
     render json: Product.all.to_json
   end
 
+  def products_1_method
+    render json: Product.first.to_json
+  end
+
+  def products_2_method
+    render json: Product.second.to_json
+  end
+
+  def products_3_method
+    render json: Product.third.to_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/products_all" => "products#products_all_method"
-
-
+  get "/products_1" => "products#products_1_method"
+  get "/products_2" => "products#products_2_method"
+  get "/products_3" => "products#products_3_method"
 end
